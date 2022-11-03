b13309c0958af968b4ece0ec77164443d9e852c4
Evgeny Gritsenko <99508836+Rufinad@users.noreply.github.com>
2022-11-03 14:35:03 +0300

    Lesson 2 (#1)
    * Preconfig
    
    * lesson_2 Hello world with CBV and FBV
    
    * lesson_2 Static site with Django
    
    * lesson_2 Code style fix

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path("", views.MainPageview.as_view()),
+    path("index.html/", views.MainPageview.as_view()),
+    path("about-us.html/", views.AboutUs.as_view()),
+    path("contact.html/", views.Contact.as_view()),
+    path("news.html/", views.News.as_view()),
+    path("rooms.html/", views.Rooms.as_view()),
+    path("services.html/", views.Services.as_view()),
+]
---
+++ mainapp/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import TemplateView
+
+
+class MainPageview(TemplateView):
+    template_name = "mainapp/index.html/"
+
+
+class AboutUs(TemplateView):
+    template_name = "mainapp/about-us.html/"
+
+
+class Contact(TemplateView):
+    template_name = "mainapp/contact.html/"
+
+
+class News(TemplateView):
+    template_name = "mainapp/news.html/"
+
+
+class Rooms(TemplateView):
+    template_name = "mainapp/rooms.html/"
+
+
+class Services(TemplateView):
+    template_name = "mainapp/services.html/"
